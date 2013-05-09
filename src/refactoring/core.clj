8de348b3d60cf614eca4a97da98ec1d2c9a5aade
Wilkes Joiner <wilkesjoiner@gmail.com>
2013-05-08 19:30:51 -0500

    extracted rental-price into multi-method

--- src/refactoring/core.clj
+++ src/refactoring/core.clj
@@ -12,23 +12,28 @@
   (Customer. (:name customer)
              (conj (:rentals customer) rental)))
 
+(defmulti rental-price (fn [rental] (-> rental :movie :price-code)))
+
+(defmethod rental-price REGULAR [rental]
+  (+ 2 (if (> (:days-rented rental) 2)
+         (* (- (:days-rented rental) 2) 1.5)
+         0)))
+
+(defmethod rental-price NEW_RELEASE [rental]
+  (* (:days-rented rental) 3))
+
+(defmethod rental-price CHILDREN [rental]
+  (+ 1.5 (if (> (:days-rented rental) 3)
+           (* (- (:days-rented rental) 3) 1.5)
+           0)))
+
 (defn statement [customer]
   (let [total-amount (atom 0)
         frequent-renter-points (atom 0)
         result (atom (str "Rental record for " (:name customer) "\n"))]
     (doseq [rental (:rentals customer)]
-      (let [amount (atom 0.0)]
-        (condp = (-> rental :movie :price-code)
-          REGULAR (do
-                    (swap! amount + 2)
-                    (when (> (:days-rented rental) 2)
-                      (swap! amount +  (* (- (:days-rented rental) 2) 1.5))))
-          NEW_RELEASE (do
-                        (swap! amount + (* (:days-rented rental) 3)))
-          CHILDREN (do
-                     (swap! amount + 1.5)
-                     (when (> (:days-rented rental) 3)
-                       (swap! amount + (* (- (:days-rented rental) 3) 1.5)))))
+      (let [amount (rental-price rental)]
+
         (swap! frequent-renter-points inc)
 
         (when (and (= (-> rental :movie :price-code) NEW_RELEASE)
@@ -36,8 +41,8 @@
           (swap! frequent-renter-points inc))
 
         (swap! result str
-               "\t" (-> rental :movie :title) "\t" @amount "\n")
-        (swap! total-amount + @amount)))
+               "\t" (-> rental :movie :title) "\t" amount "\n")
+        (swap! total-amount + amount)))
 
     (swap! result str
            "Amount owed is " @total-amount "\n"
